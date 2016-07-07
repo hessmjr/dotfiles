cc37dbb8b7095ecc3a12aae9c4d23f6a29c1b58e
mark <mark.hess2@gmail.com>
2016-07-07 12:13:50 -0500

    Add files

---
+++ startup.sh
@@ -0,0 +1,4 @@
+# test which OS is running
+# based on OS run start script for that OS
+# once startup script completes and everything installed remove all unnecessary files
+# possibly create status bar?

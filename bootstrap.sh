1f53919648c7c338418b63cc34ee10d0fb0b7484
mark <mark.hess2@gmail.com>
2016-12-21 04:16:45 -0600

    fix link

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # Execute setup file
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     || exit 1
-. "./src/setup/setup.sh"
+. "./src/setup/main.sh"
 
 # Delete this file once complete
 rm -f ${BASH_SOURCE[0]}

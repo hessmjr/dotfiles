db0df2fb79e9db14fc92455bdb7a8f9145f4aa3b
mark <mark.hess2@gmail.com>
2016-12-20 21:10:59 -0600

    add macos preferences

---
+++ src/setup/preferences/macos/trackpad.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh"
+
+
+print_in_purple "\n   Trackpad\n\n"
+
+
+# Enables tapping of mouse pad to click instead of hard press
+execute "defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true && \
+         defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1 && \
+         defaults write -g com.apple.mouse.tapBehavior -int 1 && \
+         defaults -currentHost write -g com.apple.mouse.tapBehavior -int 1" \
+    "Enable 'Tap to click'"
+
+
+# Two finger tap can be used for 'right-click'
+execute "defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true && \
+         defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -int 1 && \
+         defaults -currentHost write -g com.apple.trackpad.enableSecondaryClick -bool true && \
+         defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 0 && \
+         defaults write com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 0 && \
+         defaults -currentHost write -g com.apple.trackpad.trackpadCornerClickBehavior -int 0" \
+    "Map 'click or tap with two fingers' to the secondary click"
